9cd3aaefab20821a73e385f0078e7e98ddf51ff0
Júnior Souza - Khaled <junior.souza@ti9.com.br>
2023-05-17 14:40:09 -0300

    cria microserviço hr-worker, de recursos humanos

---
+++ hr-worker/src/test/java/org/khaled/hrworker/HrWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.khaled.hrworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
