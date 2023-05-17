30b555e1bf354bcb195cc256dd9fa6d1b971968b
Júnior Souza - Khaled <junior.souza@ti9.com.br>
2023-05-17 17:29:44 -0300

    cria rota para buscar um worker por id

--- hr-worker/src/main/java/org/khaled/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/org/khaled/hrworker/resources/WorkerResource.java
@@ -7,6 +7,7 @@ import org.khaled.hrworker.repositories.WorkerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +23,12 @@ public class WorkerResource {
         List<Worker> list = repository.findAll();
 
         return ResponseEntity.ok(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Worker> findById(@PathVariable Long id) {
+        Worker worker = repository.findById(id).get();
+
+        return ResponseEntity.ok(worker);
     }
 }
