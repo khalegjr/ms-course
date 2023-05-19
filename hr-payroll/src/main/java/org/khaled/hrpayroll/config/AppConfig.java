a7d8b14e34afad15b9576b4e67ee7000fdcb5472
Khaled <khalegjr@gmail.com>
2023-05-19 19:59:06 -0300

    wip: refatora conexão entre serviços para utilizar Feign

--- hr-payroll/src/main/java/org/khaled/hrpayroll/config/AppConfig.java
+++ hr-payroll/src/main/java/org/khaled/hrpayroll/config/AppConfig.java
@@ -2,9 +2,11 @@ package org.khaled.hrpayroll.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
+@CrossOrigin(origins = "localhost:8101", maxAge = 3600)
 public class AppConfig {
 
     @Bean
