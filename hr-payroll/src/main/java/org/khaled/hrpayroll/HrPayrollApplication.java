1bafb78706c499738a6a9d2fa3db4fb59f3ca04a
Khaled <khalegjr@gmail.com>
2023-05-23 14:58:58 -0300

    atualiza compatibilidade com spring 3.1

--- hr-payroll/src/main/java/org/khaled/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/org/khaled/hrpayroll/HrPayrollApplication.java
@@ -1,11 +1,14 @@
 package org.khaled.hrpayroll;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 
-@EnableFeignClients
 @SpringBootApplication
+@EnableFeignClients
+@ImportAutoConfiguration({FeignAutoConfiguration.class})
 public class HrPayrollApplication {
 
 	public static void main(String[] args) {
--- hr-payroll/src/main/java/org/khaled/hrpayroll/feignclients/WorkerFeignClient.java
+++ hr-payroll/src/main/java/org/khaled/hrpayroll/feignclients/WorkerFeignClient.java
@@ -3,11 +3,9 @@ package org.khaled.hrpayroll.feignclients;
 import org.khaled.hrpayroll.entities.Worker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@Component
 @FeignClient(name = "hr-worker", url = "localhost:8001", path = "/workers")
 public interface WorkerFeignClient {
 
